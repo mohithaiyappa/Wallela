08acd85e57716a868de9c6f660f2456993661897
mohithaiyappa <mohithaiyappa229@gmail.com>
2019-05-01 17:29:55 +0530

    Fabric Crashlytics. ** Added Fabric Crashlytics.

--- app/src/main/java/tk/mohithaiyappa/wallela/FrescoApplication.java
+++ app/src/main/java/tk/mohithaiyappa/wallela/FrescoApplication.java
@@ -2,6 +2,8 @@ package tk.mohithaiyappa.wallela;
 
 import android.app.Application;
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class FrescoApplication extends Application {
 
@@ -9,6 +11,7 @@ public class FrescoApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         Fresco.initialize(this);
     }
 
