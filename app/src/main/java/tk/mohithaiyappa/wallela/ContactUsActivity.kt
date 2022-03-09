2d944313869f3ec278c74bf6b862e749fad0829a
mohithaiyappa <MohithAiyappa229@gmail.com>
2022-03-09 20:48:31 +0530

    changed ContactUsActivity to use view binding

--- app/src/main/java/tk/mohithaiyappa/wallela/ContactUsActivity.kt
+++ app/src/main/java/tk/mohithaiyappa/wallela/ContactUsActivity.kt
@@ -2,24 +2,23 @@ package tk.mohithaiyappa.wallela
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import tk.mohithaiyappa.wallela.databinding.ActivityContactUsBinding
 
 class ContactUsActivity : AppCompatActivity() {
+
+    private var binding: ActivityContactUsBinding? = null
     private val recipient = arrayOf("wallela@protonmail.com")
-    private var etSubject: EditText? = null
-    private var etMessage: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contact_us)
-        val fab_mail = findViewById<FloatingActionButton>(R.id.fab_send_mail)
-        etSubject = findViewById(R.id.editText)
-        etMessage = findViewById(R.id.editText2)
-        fab_mail.setOnClickListener {
-            if (etMessage?.getText().toString().trim { it <= ' ' } != null || etSubject?.getText()
-                    .toString().trim { it <= ' ' } != null) sendMail() else {
+
+        binding = ActivityContactUsBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        binding!!.fabSendMail.setOnClickListener {
+            if (!binding!!.etMessage.text.isNullOrEmpty() || !binding!!.etSubject.text.isNullOrEmpty()) sendMail() else {
                 Toast.makeText(
                     this@ContactUsActivity,
                     "Subject or Message cannot be empty",
@@ -32,8 +31,8 @@ class ContactUsActivity : AppCompatActivity() {
     private fun sendMail() {
         val intent = Intent(Intent.ACTION_SEND)
         intent.putExtra(Intent.EXTRA_EMAIL, recipient)
-        intent.putExtra(Intent.EXTRA_SUBJECT, etSubject!!.text.toString())
-        intent.putExtra(Intent.EXTRA_TEXT, etMessage!!.text.toString())
+        intent.putExtra(Intent.EXTRA_SUBJECT, binding!!.etSubject.text.toString())
+        intent.putExtra(Intent.EXTRA_TEXT, binding!!.etMessage.text.toString())
         intent.type = "message/rfc822"
         startActivity(Intent.createChooser(intent, "Choose an email client"))
         finish()
