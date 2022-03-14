4af24349a69101b69a168b880239becea3ce08c4
mohithaiyappa <MohithAiyappa229@gmail.com>
2022-03-14 18:47:45 +0530

    added base activity wallela

---
+++ app/src/main/java/tk/mohithaiyappa/wallela/WallelaActivity.kt
@@ -0,0 +1,11 @@
+package tk.mohithaiyappa.wallela
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WallelaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wallela)
+    }
+}
